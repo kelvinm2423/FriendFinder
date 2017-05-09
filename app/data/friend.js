3ce6fd38544df7024ac2b1270c819b631f5d15ee
Kelvin Medina <kelvinm2423@gmail.com>
2017-05-08 22:14:52 -0400

    Homework Submission

---
+++ app/data/friend.js
@@ -0,0 +1,35 @@
+var friend = [
+   {
+      "name":"Kelvin",
+      "photo":"",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+   {
+      "name":"Manny",
+      "photo":"",
+      "scores":[4,3,4,1,5,2,5,3,1,4]
+   },
+   {
+      "name":"Steven",
+      "photo":"",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+   {
+      "name":"Hansel",
+      "photo":"",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+	{
+	   "name":"Mel",
+	   "photo":"",
+	   "scores":[4,2,5,1,3,2,2,1,3,2]
+	},
+      {
+      "name":"Matt",
+      "photo":"",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   }
+
+]
+
+module.exports = friend;
